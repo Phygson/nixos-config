fd3b45225550704a13045ca395ee533c8cef7f4d
phygson <amalec9999@yandex.ru>
2022-12-16 23:05:48 +0300

    Init

---
+++ home-manager/home.nix
@@ -0,0 +1,117 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{ inputs, outputs, lib, config, pkgs, ... }: {
+  # You can import other home-manager modules here
+
+  nix.package = pkgs.nix;
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    # You can also split up your configuration and import pieces of it here:
+    ./../nixos/cachix.nix
+    inputs.hyprland.homeManagerModules.default
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays your own flake exports (from overlays dir):
+      # outputs.overlays.modifications
+      # outputs.overlays.additions
+      inputs.nixpkgs-wayland.overlay
+      # Or overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
+  home = {
+    username = "phygson";
+    homeDirectory = "/home/phygson";
+  };
+  home.sessionVariables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    XDG_SESSION_TYPE = "wayland";
+    GBM_BACKEND = "nvidia-drm";
+     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+     WLR_NO_HARDWARE_CURSORS = "1";
+  }; 
+  fonts.fontconfig.enable = true;
+  
+  xdg.userDirs.enable = true;
+  xdg.userDirs.createDirectories = true;
+
+  programs.fish.enable = true;
+  programs.fish.plugins = [
+    {
+      name = "pure";
+      src = pkgs.fetchFromGitHub {
+        owner = "pure-fish";
+        repo = "pure";
+        rev = "v4.3.1";
+        sha256 = "5fefzS5XTX/8kHNnb2dqaO8raUfBhENRlZsGikWhCkM=";
+      };
+    }
+  ];
+
+  programs.kitty = {
+    enable = true;
+    font.name = "Fira Code";
+    font.package = pkgs.fira-code;
+    font.size = 12;
+    settings = {
+      shell = "fish --interactive";
+      confirm_os_window_close = "0";
+    };
+  };
+
+  xsession.enable = true;
+
+  home.packages = with pkgs; [ wofi gtklock ];
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    systemdIntegration = true;
+    extraConfig = (import ./hyprland.conf.nix {});
+  };
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    extensions = [ pkgs.vscode-extensions.jnoortheen.nix-ide ];
+  };
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git = {
+    enable = true;
+    userName  = "phygson";
+    userEmail = "124876018+Phygson@users.noreply.github.com";
+  };
+  programs.gh = {
+    enable = true;
+  };
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "22.11";
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,142 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+
+{ inputs, outputs, lib, config, pkgs, ... }: {
+  # You can import other NixOS modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/nixos):
+    # outputs.nixosModules.example
+
+    # Or modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    ./cachix.nix
+    ./hardware-configuration.nix
+
+    inputs.hyprland.nixosModules.default
+  ];
+  fileSystems = {
+    "/".options = [ "discard=async" "space_cache=v2" "compress=zstd" "noatime" ];
+    "/home".options = [ "discard=async" "space_cache=v2" "compress=zstd" "noatime" ];
+    "/nix".options = [ "discard=async" "space_cache=v2" "compress=zstd" "noatime" ];
+  };
+
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays your own flake exports (from overlays dir):
+      # outputs.overlays.modifications
+      # outputs.overlays.additions
+      inputs.nixpkgs-wayland.overlay
+      # Or overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  nix.settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Deduplicate and optimize nix store
+      auto-optimise-store = true;
+  };
+
+  # FIXME: Add the rest of your current configuration
+
+  networking.hostName = "grob";
+  networking.networkmanager.enable = true;
+
+  # TODO: This is just an example, be sure to use whatever bootloader you prefer
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.consoleMode = "max";
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  time.timeZone = "Europe/Moscow";
+  i18n.defaultLocale = "ru_RU.UTF-8";
+  console = {
+    font = "cyr-sun16";
+    keyMap = "ru";
+    #   useXkbConfig = true; # use xkbOptions in tty.
+  };
+
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  
+  security.pam.services.gtklock = {};
+
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    # gtk portal needed to make gtk apps happy
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  programs.hyprland.enable = true;
+
+  services.xserver.layout = "us,ru";
+  services.xserver.xkbOptions = "grp:alt_shift_toggle";
+
+  hardware.opengl.enable = true;
+  hardware.nvidia.nvidiaSettings = true;
+  hardware.nvidia.modesetting.enable = true;
+  hardware.opengl.driSupport32Bit = true;
+  hardware.nvidia.open = false;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    wget
+    firefox-wayland
+    git
+    github-cli
+    #home-manager
+    fish
+    any-nix-shell
+    #libreoffice
+  ];
+
+  programs.fish.enable = true;
+  programs.fish.promptInit = ''
+    any-nix-shell fish --info-right | source
+  '';
+
+  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
+  users.users = {
+    # FIXME: Replace with your username
+    phygson = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "networkmanager" ];
+    };
+  };
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "22.11";
+}

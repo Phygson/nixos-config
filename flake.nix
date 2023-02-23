d3c19f7ee8b75a0653382e092713d52ce2404a23
phygson <124876018+Phygson@users.noreply.github.com>
2023-02-23 11:01:01 +0300

    Add guvids user 3

--- flake.nix
+++ flake.nix
@@ -69,6 +69,14 @@
             ./home/phygson
           ];
         };
+	"guvids@grob" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            # > Our main home-manager configuration file <
+            ./home/guvids
+          ];
+        };
       };
     };
 }
